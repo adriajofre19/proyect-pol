8fda7c6005f7e94e358170ba29aa385cc91e0e2e
adriajofre19 <adria.ordis@gmail.com>
2025-01-24 11:40:54 +0100

    aaaa

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,11 +49,11 @@ export default function RootLayout({
       <meta property="og:title" content="Arenas Mora & Asociados | Inicio" />
       <meta property="og:description" content="Despacho de abogados multidisciplinar que ofrece soluciones jurídicas en el ámbito del Derecho, con un óptimo grado de satisfacción de sus clientes." />
       <meta property="og:url" content="https://arenasmorapol.com" />
-      <meta property="og:image" content="https://arenasmorapol.com/logo.png" />
-      <meta name="twitter:card" content="https://arenasmorapol.com/logo.png" />
+      <meta property="og:image" content="/logo.avif" />
+      <meta name="twitter:card" content="/logo.avif" />
       <meta name="twitter:title" content="Arenas Mora & Asociados | Inicio" />
       <meta name="twitter:description" content="Despacho de abogados multidisciplinar que ofrece soluciones jurídicas en el ámbito del Derecho, con un óptimo grado de satisfacción de sus clientes." />
-      <meta name="twitter:image" content="https://arenasmorapol.com/logo.png" />
+      <meta name="twitter:image" content="/logo.avif" />
       <link rel="canonical" href="https://arenasmorapol.com" />
 
       <body className={openSans.className}>
