5fca347dd4b37ab54bc0c4d3a4d5b97922b219d1
adriajofre19 <118483942+adriajofre19@users.noreply.github.com>
2025-01-25 01:09:43 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,23 +4,8 @@ import { Inter, Roboto, Lato, Open_Sans } from 'next/font/google';
 import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/components/ui/footer';
 import { CookieConsent } from '@/components/CookieConsent';
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import Head from 'next/head';
-
-const inter = Inter({ subsets: ['latin'] });
-
-const roboto = Roboto({
-  subsets: ['latin'], // Incluye el subconjunto 'latin' para el soporte básico.
-  weight: ['400', '700'], // Agrega los pesos deseados (400 para regular, 700 para bold).
-  variable: '--font-roboto', // Define una variable CSS opcional para usar más adelante.
-});
-
-const lato = Lato({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-lato',
-});
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const openSans = Open_Sans({
   subsets: ['latin'],
@@ -30,7 +15,45 @@ const openSans = Open_Sans({
 
 export const metadata: Metadata = {
   title: 'Arenas Mora & Asociados | Inicio',
-  description: 'Despacho de abogados multidisciplinar que ofrece soluciones jurídicas en el ámbito del Derecho, con un óptimo grado de satisfacción de sus clientes.',
+  description:
+    'Despacho de abogados multidisciplinar que ofrece soluciones jurídicas en el ámbito del Derecho, con un óptimo grado de satisfacción de sus clientes.',
+  keywords: [
+    'abogado',
+    'abogado barcelona',
+    'abogado laboralista',
+    'abogado laboral',
+    'abogado despidos',
+    'abogado divorcios',
+    'abogado separaciones',
+    'abogado penal',
+    'abogado penalista',
+    'despacho abogado',
+    'consulta jurídica',
+    'asesoría laboral',
+    'ley de la segunda oportunidad',
+    'cancelar deudas',
+    'asesoramiento jurídico',
+    'barcelona',
+    'derecho de familia',
+  ],
+  authors: [{ name: 'Pol Arenas Mora' }],
+  icons: {
+    icon: '/logo.avif',
+  },
+  openGraph: {
+    title: 'Arenas Mora & Asociados | Inicio',
+    description:
+      'Despacho de abogados multidisciplinar que ofrece soluciones jurídicas en el ámbito del Derecho, con un óptimo grado de satisfacción de sus clientes.',
+    url: 'https://arenasmorapol.com',
+    images: ['/logo.avif'],
+  },
+  twitter: {
+    card: 'summary',
+    title: 'Arenas Mora & Asociados | Inicio',
+    description:
+      'Despacho de abogados multidisciplinar que ofrece soluciones jurídicas en el ámbito del Derecho, con un óptimo grado de satisfacción de sus clientes.',
+    images: ['/logo.avif'],
+  },
 };
 
 export default function RootLayout({
@@ -39,30 +62,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es">
-      <meta name="description" content="Despacho de abogados multidisciplinar que ofrece soluciones jurídicas en el ámbito del Derecho, con un óptimo grado de satisfacción de sus clientes." />
-      <link rel="icon" href="/logo.avif" />
-      <meta name="keywords" content="abogado,abogado barcelona,abogado laboralista,abogado laboral,abogado despidos,abogado divorcios, abogado separaciones,
-        abogado penal, abogado penalista, despacho abogado, consulta jurídica, asesoría laboral, ley de la segunda oportunidad, cancelar deudas, asesoramiento jurídico,
-        barcelona, derecho de familia," />
-      <meta name="author" content="Pol Arenas Mora" />
-      <meta property="og:title" content="Arenas Mora & Asociados | Inicio" />
-      <meta property="og:description" content="Despacho de abogados multidisciplinar que ofrece soluciones jurídicas en el ámbito del Derecho, con un óptimo grado de satisfacción de sus clientes." />
-      <meta property="og:url" content="https://arenasmorapol.com" />
-      <meta property="og:image" content="/logo.avif" />
-      <meta name="twitter:card" content="/logo.avif" />
-      <meta name="twitter:title" content="Arenas Mora & Asociados | Inicio" />
-      <meta name="twitter:description" content="Despacho de abogados multidisciplinar que ofrece soluciones jurídicas en el ámbito del Derecho, con un óptimo grado de satisfacción de sus clientes." />
-      <meta name="twitter:image" content="/logo.avif" />
-      <link rel="canonical" href="https://arenasmorapol.com" />
-
-      <body className={openSans.className}>
+    <html lang="es" className={openSans.variable}>
+      <body>
         <Navbar />
         {children}
-        <Analytics />
-        <SpeedInsights />
         <Footer />
         <CookieConsent />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
