55ea26ce5240fb44655d85c7cc561eca8cb5565a
unknown <adria.ordis@gmail.com>
2025-01-31 15:23:43 +0100

    aaa

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -73,7 +73,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   const { data, error } = await resend.emails.send({
     from: `Nueva consulta <onboarding@resend.dev>`,
-    to: ['adria.ordis@gmail.com'],
+    to: ['polarenas00@gmail.com'],
     subject: `Nueva consulta: ${subject}`,
     html: emailContent,
   });
